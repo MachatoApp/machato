8f3e1320e09ec596be363ec7863f126749c12914
Théophile Cailliau <theophile.cailliau@gmail.com>
2023-03-24 00:43:28 +0100

    Initial Commit

---
+++ Machato/MachatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachatoApp.swift
+//  Machato
+//
+//  Created by Théophile Cailliau on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MachatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
