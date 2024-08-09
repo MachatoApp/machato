6e8d05e4ff9ebe97fc2d1a36455d44822e34d297
Emre Şafak <esafak@users.noreply.github.com>
2024-08-08 22:41:58 -0400

    Support gpt_4o_20240806 (fixes #108)

--- Machato/Data/DataTypes/Models/OpenAIModels.swift
+++ Machato/Data/DataTypes/Models/OpenAIModels.swift
@@ -16,6 +16,7 @@ enum OpenAIChatModel: String, CaseIterable, Identifiable, ModelEnum {
     case gpt_35_turbo        = "gpt-3.5-turbo"
     case gpt_4_turbo         = "gpt-4-turbo"
     case gpt_4o              = "gpt-4o"
+    case gpt_4o_20240806     = "gpt-4o-2024-08-06"
     case gpt_4o_mini         = "gpt-4o-mini"
 
     var contextLength : Int32 {
@@ -24,7 +25,7 @@ enum OpenAIChatModel: String, CaseIterable, Identifiable, ModelEnum {
             return 16_385
         case .gpt_4:
             return 8_192
-        case .gpt_4_turbo, .gpt_4o, .gpt_4o_mini:
+        case .gpt_4_turbo, .gpt_4o, .gpt_4o_20240806, .gpt_4o_mini:
             return 128_000
         }
     }
