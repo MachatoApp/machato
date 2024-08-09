38ee55c05c3d9fba0679b332063eb53c17fd898d
Emre Şafak <3928300+esafak@users.noreply.github.com>
2024-08-09 13:11:35 -0400

    feat: Refresh models (#109)
    * feat: Refresh models
    
    * Remove old ones, add new ones.
    * Update version to 1.9.5
    
    * Support gpt_4o_20240806 (fixes #108)
    
    * Incorporate @FliiFe's patch
    
    https://github.com/MachatoApp/machato/pull/109#issuecomment-2278373532
    
    ---------
    
    Co-authored-by: Emre Şafak <esafak@users.noreply.github.com>

--- Machato/Data/DataTypes/APICost.swift
+++ Machato/Data/DataTypes/APICost.swift
@@ -14,40 +14,35 @@ struct ModelCost {
     
     static let gpt3 : ModelCost = .init(sent: 0.0005, received: 0.0015)
     static let gpt4 : ModelCost = .init(sent: 0.03, received: 0.06)
-    static let gpt4_32k : ModelCost = .init(sent: 0.06, received: 0.12)
     static let gpt4_turbo : ModelCost = .init(sent: 0.01, received: 0.03)
     static let gpt4_o : ModelCost = .init(sent: 0.005, received: 0.015)
-    
+    static let gpt4_o_20240806 : ModelCost = .init(sent: 0.0025, received: 0.010)
+    static let gpt_4o_mini : ModelCost = .init(sent: 0.00015, received: 0.0006)
+
     static func getOpenAIModelCost(model: String) -> ModelCost {
         switch OpenAIChatModel.fromString(model) {
         case .gpt_35_turbo:
             return gpt3
         case .gpt_4:
             return gpt4
-        case .gpt_4_32k:
-            return gpt4_32k
         case .gpt_4_turbo:
-            return gpt4_turbo
-        case .gpt_4_turbo_preview:
-            return gpt4_turbo
-        case .gpt_4_turbo_vision:
             return gpt4_turbo
         case .gpt_4o:
             return gpt4_o
+        case .gpt_4o_20240806:
+            return gpt4_o_20240806
+        case .gpt_4o_mini:
+            return gpt_4o_mini
         }
     }
     
     static func getAnthropicModelCost(model : String) -> ModelCost {
         switch AnthropicModel.fromString(model) {
-        case .claude_instant_v1:
-            return .init(sent: 0.8 / 1_000, received: 2.4 / 1_000)
-        case .claude_v2, .claude_v2_1:
-            return .init(sent: 8.0 / 1_000, received: 24.0 / 1_000)
-        case .claude_haiku:
+        case .claude_3_haiku:
             return .init(sent: 0.25 / 1_000, received: 1.25 / 1_000)
-        case .claude_sonnet:
+        case .claude_3_sonnet, .claude_35_sonnet:
             return .init(sent: 3.0 / 1_000, received: 15.0 / 1_000)
-        case .claude_opus:
+        case .claude_3_opus:
             return .init(sent: 15.0 / 1_000, received: 75.0 / 1_000)
         }
     }
--- Machato/Data/DataTypes/Models/AnthropicModel.swift
+++ Machato/Data/DataTypes/Models/AnthropicModel.swift
@@ -8,17 +8,13 @@
 import Foundation
 
 enum AnthropicModel: String, CaseIterable, Identifiable, ModelEnum {
-    case claude_instant_v1      = "claude-instant-1.2"
-    case claude_v2              = "claude-2"
-    case claude_v2_1            = "claude-2.1"
-    case claude_haiku           = "claude-3-haiku-20240307"
-    case claude_opus            = "claude-3-opus-20240229"
-    case claude_sonnet          = "claude-3-sonnet-20240229"
+    case claude_3_haiku           = "claude-3-haiku-20240307"
+    case claude_3_opus            = "claude-3-opus-20240229"
+    case claude_3_sonnet          = "claude-3-sonnet-20240229"
+    case claude_35_sonnet         = "claude-3-5-sonnet-20240620"
 
     var contextLength : Int32 {
         switch self {
-        case .claude_instant_v1, .claude_v2:
-            return 100_000
         default:
             return 200_000
         }
@@ -29,7 +25,7 @@ enum AnthropicModel: String, CaseIterable, Identifiable, ModelEnum {
     }
     
     var id : String { return self.rawValue }
-    static let default_case : Self = .claude_haiku
+    static let default_case : Self = .claude_3_haiku
     static func fromString(_ sm: String?) -> Self {
         guard let s = sm else { return default_case }
         for c in Self.allCases {
